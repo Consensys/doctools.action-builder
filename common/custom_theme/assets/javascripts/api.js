0dd7410b6705d2f53ae2483493a78ea0dd5b80a6
Nicolas MASSART <NicolasMassart@users.noreply.github.com>
2022-04-04 16:39:03 +0200

    Fix spec doc template css (#11)
    * update redoc theme, fix sticky menu and optimise theme blocks
    * make changes previewable in this project directly

---
+++ common/custom_theme/assets/javascripts/api.js
@@ -0,0 +1,45 @@
+function getPropValue(element, name){
+  return getComputedStyle(element).getPropertyValue(name).trim()
+}
+function getTheme(element){
+  const theme = {
+    sidebar: {
+      backgroundColor: getPropValue(element, '--dt-api-nav-bg-color'),
+      textColor: getPropValue(element, '--md-default-fg-color'),
+    }
+  }
+  return theme
+}
+
+function redocInitDone(){
+  console.log("Redoc init finished!")
+}
+
+Redoc.init(
+    specurl,
+    {
+      scrollYOffset:'header',
+      theme: getTheme(document.body) },
+    document.getElementById('redoc-container'),
+    redocInitDone
+)
+
+var element = document.querySelector('body');
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (mutation.type === "attributes") {
+      Redoc.init(
+          specurl,
+          { scrollYOffset:'header',
+            theme: getTheme(mutation.target) },
+          document.getElementById('redoc-container'),
+          redocInitDone
+      )
+    }
+  });
+});
+
+observer.observe(element, {
+  attributeFilter: ['data-md-color-scheme'],
+  attributes: true //configure it to listen to attribute changes
+});
