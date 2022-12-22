afd7bb3a34f6f32fa6e92838b4605d15cb5311f1
Nicolas MASSART <nicolas.massart@consensys.net>
2022-12-22 18:24:49 +0100

    open API 3 server variables

--- mkdocs-macro-pluglets/mkdocs-macros-redoc/mkdocs_macros_redoc/__init__.py
+++ mkdocs-macro-pluglets/mkdocs-macros-redoc/mkdocs_macros_redoc/__init__.py
@@ -4,6 +4,6 @@ Code
 
 def define_env(env):
     @env.macro
-    def redoc(spec: str, fullscreen: bool = False):
-        return """<div class="redoc-container" spec-url="{spec}" fullscreen="{fullscreen}"></div>""".format(
-            spec=spec, fullscreen=str(fullscreen).lower())
+    def redoc(spec: str, fullscreen: bool = False, expand_default_server_variables: bool = False):
+        return """<div class="redoc-container" spec-url="{spec}" fullscreen="{fullscreen}" expand-default-server-variables="{expand_default_server_variables}"></div>""".format(
+            spec=spec, fullscreen=str(fullscreen).lower(), expand_default_server_variables=str(expand_default_server_variables).lower())
